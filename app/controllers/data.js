4083a0affcbfb240aad3079eec588948b77b0264
abRev <f826760951@gmail.com>
2017-03-27 12:26:24 +0800

    0.0.1版本结束

--- app/controllers/data.js
+++ app/controllers/data.js
@@ -2,11 +2,96 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
 var HomePage = mongoose.model('HomePage');
+var schedule = require('node-schedule');
+var http = require('http');
+var cheerio = require('cheerio');
+var Iconv = require('iconv').Iconv;
 
+
+//生命一个空对象用来保存定时任务对象
+var job={};
 module.exports = function(app){
 	app.use('/data',router);
 }
 
 router.get('/',function(req,res,next){
 	res.render('data/index');
+})
+
+router.post('/',function(req,res,next){
+	if("cancel" in job && typeof(job.cancel)=='function'){
+		job.cancel();
+	}
+	var timeTemp="";
+	['second','minute','hour','day','month','week'].forEach((item)=>{
+		console.log(item+": "+req.body[item]);
+		if(req.body[item]==""){
+			req.body[item]="*";
+		}
+		if(timeTemp.length>0 || req.body[item] !== '*'){
+			timeTemp+=req.body[item]+" ";
+		}
+	})
+	var time=timeTemp.substring(0,timeTemp.length-1);
+	 job=schedule.scheduleJob(time,function(){
+		console.log('进入定时器');
+	 	dynamicGetData();
+	 });
+
+	res.send('定时器启动成功');
+})
+
+function dynamicGetData(){
+	HomePage.remove({},(err)=>{
+		if(err){
+			return err;
+		}else{
+			http.get('http://www.tuyimm.com/misc.php?mod=ranklist&type=thread&view=views&orderby=all',function(_res){
+				var size=0;//接受res的长度
+				var buffers = [];
+				_res.on("data",(data)=>{
+					buffers.push(data);
+					size+=data.length;
+				});
+				_res.on('end',function(){
+					var buffer = new Buffer(size),pos=0;
+					for(var i=0;i<buffers.length;i++){
+						buffers[i].copy(buffer,pos);
+						pos+=buffers[i].length;
+					}
+					var gbkToUtf8 = new Iconv("gbk",'utf-8')
+					var html = gbkToUtf8.convert(buffer);
+					var homeObj=[];
+					var $ = cheerio.load(html);
+			
+					$("th a").each(function(){
+						var href = $(this).attr('href');
+						var name = $(this).text();
+						HomePage.findOne({name:name}).exec((err,homePage)=>{
+							if(!homePage){
+								var _homePage = new HomePage({
+									name:name,
+									html:href
+								});
+								_homePage.save();
+							}
+						});
+						homeObj.push({
+							name:name,
+							html:href
+						});
+					})
+					console.log('加载完成');
+				})
+			})
+		}
+	})
+	
+}
+
+
+router.get('/stopJob',function(req,res,next){
+
+	job.cancel();
+	res.send("yes");
 })
--- app/controllers/home.js
+++ app/controllers/home.js
@@ -10,50 +10,48 @@ module.exports = function (app) {
   app.use('/api', router);
 };
 
+router.get('/getPage',function(req,res,next){
+	http.get(req.query.remoteUrl,function(_res){
+		//接受html页面的大小
+		var size=0;
+		//保存页面的buf
+		var buffers=[];
+		_res.on('data',function(chunk){
+			buffers.push(chunk);
+			size+=chunk.length;
+		});
+		_res.on('end',function(){
+			//把buffer数组转存到一个buffer里面
+			var buffer = new Buffer(size);
+			//循环遍历时存储buffer的标记位置
+			var pos=0;
+			for(var i=0;i<buffers.length;i++){
+				buffers[i].copy(buffer,pos);
+				pos+=buffers[i].length;
+			}
+			var gbkToUtf8 = new Iconv('gbk','utf-8');
+			var html = gbkToUtf8.convert(buffer);
+			var pictures = [];
+			var $ = cheerio.load(html);
+
+			$("dl dd div img").each(function(){
+				let obj={
+					name:$(this).attr('title'),
+					src:$(this).attr('zoomfile')
+				}
+				pictures.push(obj);
+			});
+			res.json(pictures);
+		})
+	})
+})
+
 router.get('/getHome', function (req, res, next) {
 	HomePage.find(function(err,homepages){
 		if(homepages.length>0){
 			res.json(homepages)
 		}else{
-			
-			http.get('http://www.tuyimm.com/misc.php?mod=ranklist&type=thread&view=views&orderby=all',function(_res){
-				var size=0;//接受res的长度
-				var buffers = [];
-				_res.on("data",(data)=>{
-					buffers.push(data);
-					size+=data.length;
-				});
-				_res.on('end',function(){
-					var buffer = new Buffer(size),pos=0;
-					for(var i=0;i<buffers.length;i++){
-						buffers[i].copy(buffer,pos);
-						pos+=buffers[i].length;
-					}
-					var gbkToUtf8 = new Iconv("gbk",'utf-8')
-					var html = gbkToUtf8.convert(buffer);
-					var homeObj=[];
-					var $ = cheerio.load(html);
-			
-					$("th a").each(function(){
-						var href = $(this).attr('href');
-						var name = $(this).text();
-						HomePage.findOne({name:name}).exec((err,homePage)=>{
-							if(!homePage){
-								var _homePage = new HomePage({
-									name:name,
-									html:href
-								});
-								_homePage.save();
-							}
-						});
-						homeObj.push({
-							name:name,
-							html:href
-						});
-					})
-					res.json(homeObj);
-				})
-			})
+			res.json([]);
 		}
 	})
 });
